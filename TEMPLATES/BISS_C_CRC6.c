6a6d67af90ae2963925846b57561ffe3cf9c554c
kmrost <kmrost@ya.ru>
2023-02-15 10:34:28 +0400

    Update BISS_C_CRC6.c

--- TEMPLATES/BISS_C_CRC6.c
+++ TEMPLATES/BISS_C_CRC6.c
@@ -36,6 +36,6 @@ uint8_t BISS_CRC6_Calc(uint32_t data){
 	crc = CRC6_LUT[((data >> 16U) & 0xFFU) ^ crc];
 	crc = CRC6_LUT[((data >> 8U) & 0xFFU) ^ crc];
 	crc = CRC6_LUT[(data & 0xFFU) ^ crc];
-	crc = ((255U - crc) >> 2U) & 0x3FU;
+	crc = ((crc ^ 0xFFU) >> 2U) & 0x3FU;
 	return(crc);
 }
