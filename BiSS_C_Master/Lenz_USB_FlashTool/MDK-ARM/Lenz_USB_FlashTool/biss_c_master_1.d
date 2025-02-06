e6c1d448b5223ea38bf5bdd2d1dfc8ac3316626c
Kirill Rostovskiy <kmrost@lenzencoders.com>
2025-02-06 11:09:38 +0300

    moved FW project to github.com/lenzencoders/FlashToolFW

--- BiSS_C_Master/Lenz_USB_FlashTool/MDK-ARM/Lenz_USB_FlashTool/biss_c_master_1.d
+++
@@ -1,5 +0,0 @@
-lenz_usb_flashtool/biss_c_master_1.o: \
-  ..\..\lib\BiSS_C_Master\biss_c_master.c \
-  ..\..\lib\BiSS_C_Master\biss_c_master.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  ..\..\lib\BiSS_C_Master\biss_c_master_hal.h ..\Core\Inc\hw_cfg.h
---
+++ BiSS_C_Master/README.md
@@ -0,0 +1,7 @@
+# BiSS C Master register access template
+
+LEZN FlashTool firmware main repository:
+https://github.com/lenzencoders/FlashToolFW
+
+HW repository:
+https://oshwlab.com/lenz-encoders/usb_biss_c
